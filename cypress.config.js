f3f413449f12a57f32376756c03840fdb0db6dfb
Samira <samiirahassanii11@gmail.com>
2025-11-12 01:06:08 +0100

    Mes changements ou tests Cypress 1

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
 
- projectId: "tdz1rm",
+//  projectId: "tdz1rm",
+ projectId: "dpnnhv",
 
   e2e: {
     setupNodeEvents(on, config) {
